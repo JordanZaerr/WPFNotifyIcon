f96f4d878c7a55fa7f849b3147a8cf1ab71c5f0a
JordanZaerr <jordan.zaerr@extendhealth.com>
2012-10-18 12:31:57 -0600

    Updated solution to vs2010, added support for multiple custom balloons

---
+++ NotifyIconWpf/CustomBalloonInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Windows.Controls.Primitives;
+
+namespace Hardcodet.Wpf.TaskbarNotification
+{
+    public class CustomBalloonInfo
+    {
+        public object Key { get; set; }
+        public Popup Popup { get; set; }
+        public Timer CloseTimer { get; set; }
+    }
+}
